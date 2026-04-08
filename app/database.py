851b8d38597571f5929e1b13e8ff46ede2b7b984
Hasireddy <malikireddy.uma@gmail.com>
2026-04-08 12:17:12 +0200

    Database setup

---
+++ app/database.py
@@ -0,0 +1,29 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from typing import Generator
+
+# Engine: Connect to DB
+# Session: Open connection where we interact with DB
+# Base: Base class models for creating SQLAlchemy ORM
+# Model: Python Class -> DB Table
+
+
+db_url = "sqlite:///./users.db"
+engine = create_engine(db_url)
+
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db() -> Generator:
+    """
+    Dependency that provides a database session.
+
+    Yields a database session and ensures it's properly closed
+    after the request is complete.
+    """
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
