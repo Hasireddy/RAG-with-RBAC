8a29666d461956e56593789093ca6dea5f62be99
Hasireddy <malikireddy.uma@gmail.com>
2026-04-10 11:14:04 +0200

    Created models with pydantic validation

--- app/models.py
+++ app/models.py
@@ -1,7 +1,26 @@
 from pydantic import BaseModel
 
+# pydantic models for API validation
+
 class Company(BaseModel):
     id:int
     company_name:str
     domain:str
     location:str
+
+
+
+class Department(BaseModel):
+    dept_id:int
+    dept_name:str
+    company_id:int
+
+
+class Employee(BaseModel):
+    emp_id:int
+    emp_name:str
+    job_title:str
+    manager_id:int
+    dept_id:int
+    company_id:int
+
