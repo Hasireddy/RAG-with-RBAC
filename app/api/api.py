160aa65758104dd38084aa087f2e469221a5e0ee
Hasireddy <malikireddy.uma@gmail.com>
2026-04-14 14:53:02 +0200

    add department router and update API routing

--- app/api/api.py
+++ app/api/api.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
-from app.api.routes import company_route
+from app.api.routes import company_route, department_route, employee_route
 
 api_router = APIRouter()
 
 api_router.include_router(company_route.router)
+api_router.include_router(department_route.router)
+api_router.include_router(employee_route.router)
