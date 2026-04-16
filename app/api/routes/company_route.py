394a24c68a0d41867ceaf16b68a230ce0437f3d3
Hasireddy <malikireddy.uma@gmail.com>
2026-04-16 10:41:05 +0200

    feat:add company update API end point

--- app/api/routes/company_route.py
+++ app/api/routes/company_route.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
-from app.schemas.company_schema import CompanyCreate, CompanyResponse
+from app.schemas.company_schema import CompanyCreate, CompanyResponse, CompanyUpdate
 from app.database.session import get_db
 from app.models.company_model import CompanyDB
 
@@ -26,7 +26,7 @@ def create_company(company: CompanyCreate, db: Session = Depends(get_db)):
 
 # Get Company details
 @router.get("/company", response_model=CompanyResponse)
-def get_company_info(db:Session=Depends(get_db)):
+def get_company_info(db: Session = Depends(get_db)):
     company = db.query(CompanyDB).first()
 
     if not company:
@@ -39,7 +39,32 @@ def get_company_info(db:Session=Depends(get_db)):
 
 
 # Update Company details
-@router.put("/company")
-def update_company():
-    pass
+@router.put("/company/{company_id}", response_model=CompanyResponse)
+def update_company(company_id:int, company:CompanyUpdate, db: Session = Depends(get_db) ):
+    company_to_update = db.query(CompanyDB).filter(CompanyDB.id == company_id).first()
+
+    if not company_to_update:
+        raise HTTPException(
+            status_code=404,
+            detail="Company not found with given id"
+        )
+
+    if company.name is not None:
+        company_to_update.name = company.name
+
+    if company.domain is not None:
+        company_to_update.domain = company.domain
+
+    if company.location is not None:
+        company_to_update.location = company.location
+
+    if company.is_active is not None:
+        company_to_update.is_active = company.is_active
+
+    db.commit()
+    db.refresh(company_to_update)
+
+    return company_to_update
+
+
 
