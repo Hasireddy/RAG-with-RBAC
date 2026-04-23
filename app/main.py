4cadd6c6b0bcfc897e1a7e85ad3f1ec584ca126e
Hasireddy <malikireddy.uma@gmail.com>
2026-04-23 11:43:13 +0200

    add templates folder

--- app/main.py
+++ app/main.py
@@ -2,7 +2,9 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI, Request
 from dotenv import load_dotenv
 from openai import OpenAI
-from pydantic import BaseModel
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 import os
 import uvicorn
 
@@ -31,23 +33,25 @@ app = FastAPI(
     lifespan=lifespan
   )
 
-
 # Include all routes
 app.include_router(api_router)
+
+app.mount("/static", StaticFiles(directory="frontend/static"), name="static")
+templates = Jinja2Templates(directory="frontend/templates")
+
+
+
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="index.html"
+    )
+
 
 
 client = OpenAI(api_key=API_KEY)
 
-class Step(BaseModel):
-    explanation: str
-    output: str
-
-class MathReasoning(BaseModel):
-    steps: list[Step]
-    final_answer: str
-
-
-@app.get("/")
+@app.get("/root")
 def root():
     try:
        response = client.responses.create(
@@ -70,10 +74,10 @@ def root():
         return {"error": str(e)}
 
 
+
 if __name__ == "__main__":
     uvicorn.run(  "main:app",
         host="127.0.0.1",
         port=8000,
         reload=True
     )
-
