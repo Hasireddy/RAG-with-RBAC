c92903c7444ca5029094dd8c4452e298a418761d
Hasireddy <malikireddy.uma@gmail.com>
2026-05-08 14:30:43 +0200

    add login API endpoint

--- app/auth/hash_password.py
+++ app/auth/hash_password.py
@@ -3,17 +3,20 @@ from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
-def normalize_password(password: str) -> str:
+"""def normalize_password(password: str) -> str:
     # SHA256 reduces any size input to fixed 32 bytes
-    return hashlib.sha256(password.encode()).hexdigest()
+    return hashlib.sha256(password.encode()).hexdigest()"""
 
 
 def hash_password(password: str) -> str:
-    return pwd_context.hash(normalize_password(password))
+    return pwd_context.hash(password)
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
+    if not plain_password or not hashed_password:
+        return False
+
     return pwd_context.verify(
-        normalize_password(plain_password),
+        plain_password,
         hashed_password
     )
--- app/auth/jwt.py
+++ app/auth/jwt.py
@@ -1,17 +1,52 @@
+import os
+from fastapi.security import OAuth2PasswordBearer
 from datetime import datetime, timedelta, timezone
+from fastapi import Depends, HTTPException
 from typing import Optional
-from jose import jwt
+from jose import jwt, JWTError
 
 
-SECRET_KEY = "your_secret_key_change_this"
+SECRET_KEY = os.getenv("SECRET_KEY", "dev_secret_only")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
+REFRESH_TOKEN_EXPIRE_DAYS = 7
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+
 
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
 
-    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
+    expire = (
+        datetime.now(timezone.utc) + expires_delta
+        if expires_delta
+        else datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    )
+
+    to_encode.update({
+        "exp": expire,
+        "type": "access"
+    })
+
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+
+        user_id = payload.get("sub")
+
+        if not user_id:
+            raise HTTPException(status_code=401, detail="Invalid token")
+        return payload
+
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Token expired or invalid")
+
+
+
+
