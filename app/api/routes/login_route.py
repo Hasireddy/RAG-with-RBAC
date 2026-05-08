04e1a08a47d968293a27ee0485c0f1ebee094456
Hasireddy <malikireddy.uma@gmail.com>
2026-05-08 15:10:47 +0200

    add login template

--- app/api/routes/login_route.py
+++ app/api/routes/login_route.py
@@ -21,7 +21,7 @@ def login(user: LoginRequest, db: Session = Depends(get_db)):
     if not db_user:
         raise HTTPException(status_code=401, detail="Invalid credentials")
 
-    if not verify_password(user.pasword, db_user.hashed_password):
+    if not verify_password(user.password, db_user.hashed_password):
         raise HTTPException(status_code=401, detail="Invalid credentials")
 
     token = create_access_token(
