3aab8639bead920553aaca5f0c3a3eed8f4f4d3c
Uma <malikireddy.uma@gmail.com>
2026-05-15 18:41:33 +0200

    Dept_id field changed to List

--- app/models/employee_model.py
+++ app/models/employee_model.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime, Table, UniqueConstraint
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime
+from typing import List
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from app.database.base import Base
@@ -21,7 +22,7 @@ class EmployeeDB(Base):
 
     # Foreign key: Each employee belongs to a department and each department belongs to a company
     company_id = Column(Integer, ForeignKey("companies.id"), nullable=False, index=True)
-    dept_id = Column(Integer, ForeignKey("departments.id", ondelete="CASCADE"), index=True, nullable=False)
+    dept_id = Column(List, ForeignKey("departments.id", ondelete="CASCADE"), index=True, nullable=False)
     role_id = Column(Integer, ForeignKey("roles.id"), index=True, nullable=True) # Change this to nullable=False later when creating roles
 
     # Relationship: Employee is in Department and in a Company
