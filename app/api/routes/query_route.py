909ac8f51433eccbcbbcb362648f4bb471a26f6b
Uma <malikireddy.uma@gmail.com>
2026-05-17 11:08:39 +0200

    modified user-details template with departments display

--- app/api/routes/query_route.py
+++ app/api/routes/query_route.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
+import uuid
 
 from app.schemas.response_schema import ResponseSchema
 from app.schemas.query_schema import QueryRequest
@@ -11,6 +12,7 @@ from app.database.session import get_db
 from app.rag_utils.query_classifier import detect_query_type_llm
 from app.schemas.query_schema import QueryRequest
 from app.schemas.response_schema import ResponseSchema
+from app.auth.jwt import get_current_user
 
 
 # Load environment variables
@@ -23,15 +25,29 @@ client = OpenAI(api_key=API_KEY)
 
 templates = Jinja2Templates(directory="frontend/templates")
 
-
+#def chat(payload: QueryRequest, db: Session = Depends(get_db), user:dict = Depends(get_current_user)):
 @router.post("/chat", response_model=ResponseSchema)
 def chat(payload: QueryRequest, db: Session = Depends(get_db)):
     query = payload.query.strip()
+
+    if not query:
+        raise HTTPException(
+            status_code=400,
+            detail="Query cannot be empty"
+        )
+
+    # Session handling
+    #session_id = payload.session_id
+
+    #if not session_id:
+        #session_id = str(uuid.uuid4())
+
     #Detect mode
     mode = detect_query_type_llm(query)
     ai_answer = mode
     print(f"Detected mode:{mode}")
     return {
+        #"session_id": session_id,
         "session_id": "1",
         "answer": ai_answer,
         "mode": mode
